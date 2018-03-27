4b30eddc11a837a759af8bf09f013e8d85ac8524
kpgoodale <kpgoodale@gmail.com>
2018-03-27 01:33:03 -0400

    Added hover information on map markers

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -17,11 +17,30 @@ class Map extends Component {
 
         // Configure the markers
         this.props.meters.forEach((meter) => {
-            new window.google.maps.Marker({
+            let marker = new window.google.maps.Marker({
                 map: map,
                 position: {lat: meter.latitude, lng: meter.longitude}
-            }).addListener('click', (marker) => {
+            });
+            let infoWindowContent = "<ul class='infoWindow'>"
+                + "<li> ID: " + meter.id + "</li>"
+                + "<li> Latitude: " + meter.latitude + "</li>"
+                + "<li> Longitude: " + meter.longitude + "</li>"
+                + "<li> Status: " + meter.status + "</li>"
+                + "<li> Postal Code: " + meter.postal_code + "</li>"
+                + "<li> Location Description: " + meter.location_description + "</li>"
+                + "<li> Installed At: " + meter.installed_at + "</li>"
+                + "</ul>";
+            let infoWindow = new window.google.maps.InfoWindow({
+                content: infoWindowContent
+            });
+            marker.addListener('click', () => {
                 this.props.changeData(meter.id);
+            });
+            marker.addListener('mouseover',()=> {
+                infoWindow.open(map,marker);
+            });
+            marker.addListener('mouseout',()=> {
+                infoWindow.close();
             });
         });
       }
